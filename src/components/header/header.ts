58b71613b05f5c3e494f6dc3fbd62fc388852f8f
Ariadna Frias <ariadna.812@gmail.com>
2023-02-03 22:18:52 +0100

    Add header ts file

---
+++ src/components/header/header.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { Component } from '../component/component';
+import './header.scss';
+
+export class Header extends Component {
+  constructor(public selector: string) {
+    super();
+    this.template = this.createTemplate();
+    this.render('afterbegin');
+  }
+
+  private createTemplate() {
+    return `
+      <header class="header">
+        <h1 class="header__title">Pokémon App</h1>
+        <p class="header__phrase" role="phrase">Know all of them!</p>
+      </header>
+    `;
+  }
+
+  render(place: globalThis.InsertPosition) {
+    super.render(place);
+  }
+}
