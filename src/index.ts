c46ff95ec6696981f503e60b708aab8af87e7a8a
Ariadna Frias <ariadna.812@gmail.com>
2023-02-04 20:12:02 +0100

    Update index ts file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-new */
 import { Header } from './components/header/header';
-import { Menu } from './menu/menu';
-import { MenuOptions } from './models/menuOptions';
+import { Menu } from './components/menu/menu';
+
 import './index.scss';
 
-const menuOptions: MenuOptions[] = [
-  { label: 'Home', path: '/home' },
-  { label: 'My favorite', path: '/favorite' },
-];
-
 new Header('.app');
-new Menu('.header', menuOptions);
+new Menu('.header');
