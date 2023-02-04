61e5d64b02582dc2cffdeafa9cc41ce2ce4c881f
Ariadna Frias <ariadna.812@gmail.com>
2023-02-04 20:04:16 +0100

    Add header test file

--- src/components/header/header.test.ts
+++ src/components/header/header.test.ts
@@ -2,14 +2,18 @@ import { screen } from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import { Header } from './header';
 
-describe('Given Header component', () => {
-  test('should first', () => {
-    document.body.innerHTML = '<slot></slot>';
-    const element = new Header('slot');
-    expect(element).toBeInstanceOf(Header);
-    const h1 = screen.getByRole('heading');
-    expect(h1).toBeInTheDocument();
-    const p = screen.getByRole('phrase');
-    expect(p).toBeInTheDocument();
+describe('Given a Header component', () => {
+  describe('When we instance a Header class', () => {
+    test('the element should be a instance of Header', () => {
+      document.body.innerHTML = '<slot></slot>';
+      const element = new Header('slot');
+      expect(element).toBeInstanceOf(Header);
+    });
+    test('the roles of h1 and p should be the heading and a phrase', () => {
+      const h1 = screen.getByRole('heading');
+      expect(h1).toBeInTheDocument();
+      const p = screen.getByRole('phrase');
+      expect(p).toBeInTheDocument();
+    });
   });
 });
