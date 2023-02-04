68dfe19a9b02db60c471fac28bc75de94ba9cc01
Ariadna Frias <ariadna.812@gmail.com>
2023-02-04 23:30:35 +0100

    Update index ts file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 /* eslint-disable no-new */
 import { Header } from './components/header/header';
 import { Menu } from './components/menu/menu';
-import { Card } from './components/card/card';
 
 import './index.scss';
 
