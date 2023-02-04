90a701c7f9b7f33f5bfa5a21e8b22dea7623f320
Ariadna Frias <ariadna.812@gmail.com>
2023-02-04 20:00:48 +0100

    Add menu ts file

--- src/components/menu/menu.ts
+++ src/components/menu/menu.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
-import { MenuOptions } from '../models/menuOptions';
-import { Component } from '../components/component/component';
+
+import { Component } from '../component/component';
 import './menu.scss';
 
 export class Menu extends Component {
-  constructor(public selector: string, public menuOptions: MenuOptions[]) {
+  constructor(public selector: string) {
     super();
     this.template = this.createTemplate();
     this.render('beforeend');
@@ -12,7 +12,7 @@ export class Menu extends Component {
 
   createTemplate() {
     return `
-    <nav class="menu">
+    <nav class="menu" role="menu">
     <ul>
     <li class= "menu__home"><a href="home.path">Home</a></li>
     <li class="menu__favorite"><a href="favorite.path">My favorite</a></li>
