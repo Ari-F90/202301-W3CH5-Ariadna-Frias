fa1808eda37ec8c613feadaf80e64a009aff6269
Ariadna Frias <ariadna.812@gmail.com>
2023-02-05 11:19:16 +0100

    Update index ts file

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@
 /* eslint-disable no-new */
 import { Header } from './components/header/header';
 import { Menu } from './components/menu/menu';
+import { PokeList } from './components/section/pokeList';
+import { Footer } from './components/footer/footer';
 
+import { PokeApiRepo } from './services/repository/poke.api.repo';
 import './index.scss';
 
+const poke = await new PokeApiRepo().loadPokemons();
 new Header('.app');
 new Menu('.header');
+new PokeList('header');
+new Footer('section');
